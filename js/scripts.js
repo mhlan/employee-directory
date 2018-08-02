21e4e08c2af44ba4a53d44dc20d7598aada9b81d
Matt Lanin <mhlanin@gmail.com>
2018-08-02 12:54:18 -0700

    API work
    Able to load and insert relevant API data into first card element.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 //constant variables
 const main = document.querySelector("#main");
+const boxes = document.querySelectorAll(".box");
 const modalWindow = document.querySelector(".modal");
 const modalBG = document.querySelector(".modal-background");
 const modalClose = document.querySelector(".modal-close");
+const images = document.querySelectorAll(".img");
+const names = document.querySelectorAll(".subtitle.is-3");
+const emails = document.querySelectorAll(".email");
+const locations = document.querySelectorAll(".location");
 
 //toggle modal window on
 main.addEventListener("click", e => {
@@ -27,3 +32,33 @@ modalClose.addEventListener("click", () =>
 modalBG.addEventListener("click", () =>
   modalWindow.classList.remove("is-active")
 );
+
+fetch("https://randomuser.me/api/")
+  .then(checkStatus)
+  .then(res => res.json())
+  .then(data => {
+    const picture = data.results[0].picture.large;
+    const firstName = data.results[0].name.first;
+    const lastName = data.results[0].name.last;
+    const email = data.results[0].email;
+    const city = data.results[0].location.city;
+    const state = data.results[0].location.state;
+
+    fillDirectory(firstName, lastName, picture, email, city, state);
+  })
+  .catch(error => console.log("Looks like there was a problem", error));
+
+function checkStatus(response) {
+  if (response.ok === true) {
+    return Promise.resolve(response);
+  } else {
+    return Promise.reject(new Error(response.statusText));
+  }
+}
+
+function fillDirectory(firstName, lastName, picture, email, city, state) {
+  images[0].src = picture;
+  names[0].innerHTML = `${firstName} ${lastName}`;
+  emails[0].innerHTML = email;
+  locations[0].innerHTML = `${city}, ${state}`;
+}
