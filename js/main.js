ffbef29694b45003cfbc46e492213d140eb690e7
Matt Lanin <mhlanin@gmail.com>
2018-09-10 12:01:30 -0700

    Refactored everything
    Refactored everything from square one, now the API properly loads 12 employees and creates/writes  appropriate HTML elements.

---
+++ js/main.js
@@ -0,0 +1,114 @@
+// (function() {
+//constant variables
+const main = document.querySelector("#main");
+const modalWindow = document.querySelector(".modal");
+const modalBG = document.querySelector(".modal-background");
+const modalClose = document.querySelector(".modal-close");
+const col1 = document.querySelector("#col-1");
+const col2 = document.querySelector("#col-2");
+const col3 = document.querySelector("#col-3");
+const col4 = document.querySelector("#col-4");
+
+let boxes;
+
+//functions
+const createHTML = (picture, firstName, lastName, email, city, state) => {
+  let HTML = `<div class="column">
+        <div class="box">
+          <div class="media">
+            <div class="media-left">
+              <figure class="image is-128x128">
+                <img class="img is-rounded" src="${picture}">
+              </figure>
+            </div>
+            <div class="media-content">
+              <p class="subtitle is-4">${firstName + " " + lastName}</p>
+              <div class="content">
+                <div class="email">${email}</div>
+                <div class="location">${city + ", " + state}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`;
+
+  return HTML;
+};
+
+const writeHTML = (colNum, picture, firstName, lastName, email, city, state) =>
+  (colNum.innerHTML += createHTML(
+    picture,
+    firstName,
+    lastName,
+    email,
+    city,
+    state
+  ));
+
+const capitalize = word => word.charAt(0).toUpperCase() + word.slice(1);
+
+//api fetch
+fetch("https://randomuser.me/api/?results=12&nat=us&exc=login,registered,id")
+  .then(checkStatus)
+  .then(res => res.json())
+  .then(data => {
+    const employeeData = data.results;
+    const employeeCount = employeeData.length;
+
+    for (let i = 0; i < employeeCount; i++) {
+      const picture = employeeData[i].picture.large;
+      const firstName = capitalize(employeeData[i].name.first);
+      const lastName = capitalize(employeeData[i].name.last);
+      const email = employeeData[i].email;
+      const city = capitalize(employeeData[i].location.city);
+      const state = capitalize(employeeData[i].location.state);
+
+      if (i < 3) {
+        writeHTML(col1, picture, firstName, lastName, email, city, state);
+      } else if (i < 6) {
+        writeHTML(col2, picture, firstName, lastName, email, city, state);
+      } else if (i < 9) {
+        writeHTML(col3, picture, firstName, lastName, email, city, state);
+      } else if (i < 12) {
+        writeHTML(col4, picture, firstName, lastName, email, city, state);
+      }
+    }
+    boxes = document.getElementsByClassName("column");
+  })
+  .catch(error => console.log("Looks like there was a problem.", error));
+
+function checkStatus(response) {
+  if (response.ok === true) {
+    return Promise.resolve(response);
+  } else {
+    return Promise.reject(new Error(response.statusText));
+  }
+}
+
+const modalView = () => modalWindow.classList.add("is-active");
+
+//event listeners
+
+// main.addEventListener("click", e => {
+//   console.log(this);
+//   let selection = e.target.classList.value;
+//   if (
+//     selection === "box" ||
+//     selection === "img is-rounded" ||
+//     selection === "subtitle is-3" ||
+//     selection === "email" ||
+//     selection === "location"
+//   ) {
+//     modalWindow.classList.add("is-active");
+//   }
+// });
+
+//closes modal window on clicking "x" button
+modalClose.addEventListener("click", () =>
+  modalWindow.classList.remove("is-active")
+);
+//closes modal window on clicking anywhere but modal content box
+modalBG.addEventListener("click", () =>
+  modalWindow.classList.remove("is-active")
+);
+// })();
--- readme.md
+++ readme.md
@@ -2,15 +2,15 @@
 
 ###### API Usage
 
-- [ ] 12 random users are pulled from the the API
-- [ ] New random employee information displays each time the page refreshes
+- [x] 12 random users are pulled from the the API
+- [x] New random employee information displays each time the page refreshes
 
 ###### User Directory - The directory includes the following:
 
-- [ ] Employee Image
-- [ ] First and Last Name
-- [ ] Email
-- [ ] City or location
+- [x] Employee Image
+- [x] First and Last Name
+- [x] Email
+- [x] City or location
 - [ ] Exceeds: Employees can be filtered by name or username
 
 ###### Modal Window - Modal window displays at least the following details:
@@ -21,9 +21,9 @@
 - [ ] City or location
 - [ ] Cell Number
 - [ ] Detailed Address, including street name and number, state or country, and post code.
-- [ ] There is a way to close the modal window
+- [x] There is a way to close the modal window
 - [ ] Exceeds: Functionality has been added to switch back and forth between employees when the detail modal window is open.
 
 ###### Matching the Mockups
 
-- [ ] Directory and modal window have been styled so that all the major elements are in place and roughly matches the mockups
+- [x] Directory and modal window have been styled so that all the major elements are in place and roughly matches the mockups
